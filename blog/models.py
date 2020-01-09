544ad02bf86244fd8e1d0685114135d15827a7b7
Kr4v3n <fafachen37@gmail.com>
2020-01-09 15:19:25 +0100

    Adding comment models and forms

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.content)
